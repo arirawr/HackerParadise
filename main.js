09e21fb8b83f83943a79781fddc18767426dee7f
danstepanov <danhstepanov@gmail.com>
2016-08-08 13:23:45 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+jQuery(function($) {'use strict',
+
+	//Countdown js
+	$("#countdown").countdown({
+			date: "2 october 2016 6:00:00",
+			format: "on"
+	},
+
+	function() {
+			// callback function
+	});
+});
